3f8cf0022dcc5f4fab2679fd3f04e852483f9319
40858607 <40858607@live.napier.ac.uk>
2026-09-15 21:27:20 +0630

    Configure Docker MongoDB CI

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,38 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
+
+        // Get the database
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+
+        // Get the collection
+        MongoCollection<Document> collection = database.getCollection("test");
+
+        // Create a document
+        Document doc = new Document("name", "")
+                .append("class", "DevOps")
+                .append("year", "2026")
+                .append("result", new Document("CW", 95).append("EX", 85));
+
+        // Add document to collection
+        collection.insertOne(doc);
+
+        // Check document in collection
+        Document myDoc = collection.find().first();
+
+        System.out.println(myDoc.toJson());
+
+        // Close connection
+        mongoClient.close();
     }
 }
